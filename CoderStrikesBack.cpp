d4f1487a6cdffb0d246f8060e05192069ead67b1
Yann <26348667+Mrplobi@users.noreply.github.com>
2019-01-19 15:34:58 +0100

    first comit and entering bois 1

---
+++ CoderStrikesBack.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+int main()
+{
+
+	// game loop
+	while (1) {
+		int x;
+		int y;
+		int nextCheckpointX; // x position of the next check point
+		int nextCheckpointY; // y position of the next check point
+		int nextCheckpointDist; // distance to the next checkpoint
+		int nextCheckpointAngle; // angle between your pod orientation and the direction of the next checkpoint
+		cin >> x >> y >> nextCheckpointX >> nextCheckpointY >> nextCheckpointDist >> nextCheckpointAngle; cin.ignore();
+		int opponentX;
+		int opponentY;
+		cin >> opponentX >> opponentY; cin.ignore();
+
+		// Write an action using cout. DON'T FORGET THE "<< endl"
+		// To debug: cerr << "Debug messages..." << endl;
+		int thrust;
+		if (nextCheckpointDist < 600)
+		{
+			thrust = 30;
+		}
+		else
+		{
+			if (nextCheckpointAngle > 20 || nextCheckpointAngle < -20)
+			{
+				thrust = 10;
+			}
+			else
+			{
+				thrust = 100;
+			}
+		}
+
+
+		// You have to output the target position
+		// followed by the power (0 <= thrust <= 100)
+		// i.e.: "x y thrust"
+		cout << nextCheckpointX << " " << nextCheckpointY << " " << thrust << endl;
+	}
+}
