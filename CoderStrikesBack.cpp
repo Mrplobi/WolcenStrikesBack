f12f64e0897eefc431dc049c8363c2c77fa31898
Yann <26348667+Mrplobi@users.noreply.github.com>
2019-01-20 20:22:56 +0100

    escaping ligue bois 1 and checkpoint detection

--- CoderStrikesBack.cpp
+++ CoderStrikesBack.cpp
@@ -5,6 +5,52 @@
 
 using namespace std;
 
+enum Turns{FirstTurn, OtherTurns};
+//Class made to contain info about checkpoints, allowing me to store the checkpoints seen
+class Checkpoint
+{
+public:
+	Checkpoint(int x, int y);
+	void setX(int x);
+	int getX();
+	void setY(int y);
+	int getY();
+	bool Compare(int x, int y);
+private:
+	int xCoordinate;
+	int yCoordinate;
+};
+
+Checkpoint::Checkpoint(int x, int y) : xCoordinate(x), yCoordinate(y) {}
+
+void Checkpoint::setX(int x)
+{
+	xCoordinate = x;
+}
+int  Checkpoint::getX()
+{
+	return xCoordinate;
+}
+void Checkpoint::setY(int y)
+{
+	yCoordinate = y;
+}
+int  Checkpoint::getY()
+{
+	return yCoordinate;
+}
+
+bool Checkpoint::Compare(int x, int y)
+{
+	if (x == xCoordinate && y == yCoordinate)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
@@ -24,30 +70,65 @@ int main()
 		int opponentX;
 		int opponentY;
 		cin >> opponentX >> opponentY; cin.ignore();
-
+		bool boostAvailable = true;
+		bool boostOn = false;
+		int thrust;
+		Turns turn = FirstTurn;
+		vector<Checkpoint> checkpoints;
 		// Write an action using cout. DON'T FORGET THE "<< endl"
 		// To debug: cerr << "Debug messages..." << endl;
-		int thrust;
-		if (nextCheckpointDist < 600)
+
+		//Creates a vector containing every checkpoint and it's location
+		if (turn = FirstTurn)
 		{
-			thrust = 30;
-		}
-		else
-		{
-			if (nextCheckpointAngle > 20 || nextCheckpointAngle < -20)
+			if (checkpoints.size == 0)
 			{
-				thrust = 10;
+				checkpoints.add(new Checkpoint(nextCheckpointX, nextCheckpointY);
 			}
 			else
 			{
-				thrust = 100;
+				for each (Checkpoint checkpoint in checkpoints)
+				{
+					exist = false;
+					exist = checkpoint.Compare(nextCheckpointX, nextCheckpointY);
+					if (exist = true && checkpoint != checkpoints.back)
+					{
+						turn = OtherTurns;
+					}
+				}
 			}
 		}
 
+		if (nextCheckpointDist > 10000 && boostAvailable == true && nextCheckpointAngle < 10 && nextCheckpointAngle > -10)
+		{
+			boostAvailable = false;
+			boostOn = true;
+		}
+
+		if (boostOn == true && nextCheckpointDist < 1000)
+		{
+			boostOn = false;
+		}
+
+		if (nextCheckpointAngle > 90 || nextCheckpointAngle < -90)
+		{
+			thrust = 0;
+		}
+		else
+		{
+			thrust = 100;
+		}
 
 		// You have to output the target position
 		// followed by the power (0 <= thrust <= 100)
 		// i.e.: "x y thrust"
-		cout << nextCheckpointX << " " << nextCheckpointY << " " << thrust << endl;
+		if (boostOn == true)
+		{
+			cout << nextCheckpointX << " " << nextCheckpointY << " " << "BOOST" << endl;
+		}
+		else
+		{
+			cout << nextCheckpointX << " " << nextCheckpointY << " " << thrust << endl;
+		}
 	}
 }
